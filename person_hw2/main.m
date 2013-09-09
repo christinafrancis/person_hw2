8f6f4138a918abc97d5d11afa34bc3fe5aabc77c
Christina Francis <christina@unknown881fa11d5346.gateway.2wire.net>
2013-09-09 00:35:05 -0700

    Initial Commit

---
+++ person_hw2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  person_hw2
+//
+//  Created by Christina Francis on 9/9/13.
+//  Copyright (c) 2013 Christina Francis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
